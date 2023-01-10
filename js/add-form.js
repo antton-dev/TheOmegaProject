8d5a275ce2e2c0e5fe6dfb7583ffac6d65707398
anttondev <antton.chevrier@gmail.com>
2023-01-10 22:44:21 +0100

    adding workflow and publish a issue for contribution

---
+++ js/add-form.js
@@ -0,0 +1,9 @@
+let str = document.location.href;
+let url = new URL(str);
+
+
+let id = url.searchParams.get('id')
+let city = url.searchParams.get('ville')
+let country = url.searchParams.get('country')
+
+document.querySelector('#title').value = '[contribution] ' + city + ' - ' + id
